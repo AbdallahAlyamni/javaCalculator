e4946d28149c6a6728ff26c357a639acbb57ee1b
you <you@example.com>
2019-11-29 02:26:43 -0800

    I'm Ajabain and created the base strucutre for project @2:26 PM.

---
+++ Calculator.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+class Calculator{
+	public static void main(String args[]){
+
+	Scanner input = new Scanner(System.in);
+	int firstNumber;
+	int secondNumber;
+	int theerdNumber;
+
+	System.out.println("Write first numbre");
+	firstNumber = Integer.parseInt(input.nextLine());
+
+	System.out.println("Write second numbre");
+	secondNumber = Integer.parseInt(input.nextLine());
+
+	System.out.println("Write theerd numbre");
+	theerdNumber = Integer.parseInt(input.nextLine());
+
+	Addition(firstNumber,secondNumber,theerdNumber);
+	Substratction(firstNumber,secondNumber,theerdNumber);
+	Multiplication(firstNumber,secondNumber,theerdNumber);
+	}//End main function
+
+
+		//Addition function
+		static void Addition(int firstNumber , int secondNumber , int theerdNumber){
+			System.out.println("Addition of number equal"+ " " + (firstNumber + secondNumber + theerdNumber));
+		}//End of Addition function
+
+		//Substratction function
+		static void Substratction(int firstNumber , int secondNumber , int theerdNumber){
+			System.out.println("Substratction of number equal"+" "+(firstNumber - secondNumber - theerdNumber));
+		}//End of Substratction function
+
+		//Substratction function
+		static void Multiplication(int firstNumber , int secondNumber , int theerdNumber){
+			System.out.println("Multiplication of number equal"+" "+(firstNumber * secondNumber * theerdNumber));
+	}//End of Multiplication function
+}//End of Calculator class
